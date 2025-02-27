fcd195a154bd0c0854fd1116c62f3efea3c6acea
CoSphere24 <cosphere24@gmail.com>
2025-02-26 21:06:29 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import FormApp from '../js/FormApp';
+import FormApp from './FormApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
