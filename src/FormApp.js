91219c279d3895566dca4aa4a9b9f706081d4644
CoSphere24 <cosphere24@gmail.com>
2025-02-26 22:02:07 -0500

    Update FormApp.js

--- src/FormApp.js
+++ src/FormApp.js
@@ -4,7 +4,7 @@ import './bootstrap.css';
 import React, { useRef, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import './style.css';
+//import './style.css';
 import { Container, FloatingLabel, Row, Col } from 'react-bootstrap';
 
 function FormApp() {
