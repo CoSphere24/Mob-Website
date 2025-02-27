f61a236730e458445b90f42201903a706b3723c4
CoSphere24 <cosphere24@gmail.com>
2025-02-27 14:16:41 -0500

    Update submit-event.js

--- src/submit-event.js
+++ src/submit-event.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './bootstrap.min.css';
 import FormApp from './FormApp';
-// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <FormApp />
-  </React.StrictMode>
-);
+//function NavigationBar() { //testing
+  // TODO: Actually implement a navigation bar
+//  return <h1>Hello from React!</h1>;
+//}
+
+const domNode = document.getElementById('formapp');
+const root = createRoot(domNode);
+root.render(<FormApp />);
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+//  <React.StrictMode>
+//    <FormApp />
+//  </React.StrictMode>
+//);
