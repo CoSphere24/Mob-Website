378b905406509da2529fab463250a1d6cdf2df08
CoSphere24 <cosphere24@gmail.com>
2025-03-01 17:03:19 -0500

    Update Blogs.js

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,18 +1,5 @@
 const Blogs = () => (
       <div className='blogs'>
-          <head>
-              <meta charset="utf-8">
-              <title>Mob Blogs</title>
-              <meta name="description" content="View the latest from Mob." />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              {/* Bootstrap 4 */} 
-              <link rel="stylesheet" href="../src/bootstrap.min.css">
-              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-              {/* icons */} 
-              <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
-              {/* favicon */} 
-              <link rel="icon" type="image/jpg" href="../images/favicon.png"/>
-          </head>
           <body>
               {/* header */} 
               <nav style="background-color: #ff7842;" class="navbar navbar-expand-md navbar-dark fixed-top sticky-navigation">
