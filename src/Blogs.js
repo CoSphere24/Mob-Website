8ff9783c60369b3c48fad4e4f87ac3b574f4a30c
CoSphere24 <cosphere24@gmail.com>
2025-03-01 18:06:36 -0500

    Update Blogs.js

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,9 +1,8 @@
 const Blogs = () => (
       <div className='blogs'>
-      
-          <body>
-            <img class="img-fluid mx-auto d-block" style="padding-right: 30px" src="https://cdn3d.iconscout.com/3d/premium/thumb/camera-3d-icon-download-in-png-blend-fbx-gltf-file-formats--dslr-digital-photography-photo-device-summer-holiday-pack-holidays-icons-5058701.png"/>
-          </body>
+        <h1>Welcome to my portfolio website</h1>
+        <p> Feel free to browse around and learn more about me.</p>
+      </div>
     );
 
 
