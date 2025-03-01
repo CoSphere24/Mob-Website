d088c62654a3df0c15ce704d3ab798ba5ec3eb8e
CoSphere24 <cosphere24@gmail.com>
2025-03-01 15:32:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
     import React from 'react';
     import './App.css';
     import { NavLink, Routes, Route } from 'react-router-dom';
+    import About from './About';
 
     const App = () => (
       <div className='app'>
@@ -24,14 +25,6 @@
       <div className='home'>
         <h1>Welcome to my portfolio website</h1>
         <p> Feel free to browse around and learn more about me.</p>
-      </div>
-    );
-
-    const About = () => (
-      <div className='about'>
-        <h1>About Me</h1>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
       </div>
     );
 
