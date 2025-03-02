3bc4d0a83cf37503ecfa840e107cf5261a5ab79c
CoSphere24 <cosphere24@gmail.com>
2025-03-02 00:14:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,34 @@
     );
 
     const Navigation = () => (
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top sticky-navigation">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="ion-grid icon-sm"></span>
+            </button>
+            <a class="navbar-brand hero-heading" href="https://findyourmob.netlify.app/">Mob</a>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item mr-3">
+                        <NavLink exact activeClassName="current" to='/'><a class="nav-link page-scroll">Product <span class="sr-only">(current)</span></a></NavLink>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link page-scroll" href="#features">Features</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link page-scroll" href="#pricing">Pricing</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link page-scroll" href="#partners">Partners</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link page-scroll" href="https://findyourmob.netlify.app/blogs">Blog</a>
+                    </li>
+                    <li class="nav-item mr-3">
+                        <a class="nav-link page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
       <nav>
         <ul>
           <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
