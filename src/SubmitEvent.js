59c16d7c94e7fb68c73143ecc4aee4849ae0a383
CoSphere24 <cosphere24@gmail.com>
2025-03-03 09:40:31 -0500

    Update SubmitEvent.js

--- src/SubmitEvent.js
+++ src/SubmitEvent.js
@@ -2,25 +2,16 @@ import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import './bootstrap.min.css';
-import FormApp from './FormApp';
+import EventFormApp from './EventFormApp';
 import './bootstrap.min.css';
 import './bootstrap.css';
-
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-//import './style.css';
-import { Container, FloatingLabel, Row, Col } from 'react-bootstrap';
-
-//const domNodeForm = document.getElementById('formapp');
-//const rootForm = createRoot(domNodeForm);
-//rootForm.render(<FormApp />);
 
 const SubmitEvent = () => (
       <div className='SubmitEvent'>
         <section class="bg-texture hero">
         </section>
             <div class="container">
-                  <FormApp/>
+                  <EventFormApp/>
             </div>
       </div>
     );
