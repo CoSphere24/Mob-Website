00e73562092739dada3fd418096917d14cf19d15
CoSphere24 <cosphere24@gmail.com>
2025-03-03 00:45:51 -0500

    Update SubmitEvent.js

--- src/SubmitEvent.js
+++ src/SubmitEvent.js
@@ -19,7 +19,9 @@ const SubmitEvent = () => (
       <div className='SubmitEvent'>
         <section class="bg-texture hero">
         </section>
-            <FormApp/>
+            <div class="container">
+                  <FormApp/>
+            </div>
       </div>
     );
 
