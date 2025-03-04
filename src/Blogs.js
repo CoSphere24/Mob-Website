0f050e2c4f2e5bd927a1a31cad07f1e48ddf6606
CoSphere24 <cosphere24@gmail.com>
2025-03-03 23:53:25 -0500

    Update Blogs.js

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,7 +1,9 @@
 const Blogs = () => (
-      <div className='blogs'>
-        <h1>Welcome to my portfolio website</h1>
-        <p> Feel free to browse around and learn more about me.</p>
+      <div className='Blogs'>
+            <section class="bg-light">
+                  <br/><br/>
+                  
+            </section>
       </div>
     );
 
