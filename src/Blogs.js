90ab29fa072671171e6ee4bdb900f1e388f1de3c
CoSphere24 <cosphere24@gmail.com>
2025-03-03 23:55:16 -0500

    Update Blogs.js

--- src/Blogs.js
+++ src/Blogs.js
@@ -2,7 +2,69 @@ const Blogs = () => (
       <div className='Blogs'>
             <section class="bg-light">
                   <br/><br/>
-                  
+                  <div class="container">
+                      <div class="row">
+                          <div class="col-md-6 offset-md-3 col-sm-8 offset-sm-2 col-xs-12 text-center">
+                              <h2 class="text-primary">From Our Blog</h2>
+                              <p class="lead pt-3">
+                                  The latest news from Mob blog.
+                              </p>
+                          </div>
+                      </div>
+                      <div class="row d-md-flex mt-5">
+                          <div class="col-sm-4 mt-2 wow fadeIn">
+                              <div class="card">
+                                  <img class="card-img-top" src="https://media.istockphoto.com/id/517131390/photo/happy-young-friends-partying-with-drinks-and-confetti-in-nightclub.jpg?s=612x612&w=0&k=20&c=t0bhMdoWYz2XQiimDfVBJgiRvkqbwx-g8SH9oMFGpyk=" alt="Image">
+                                  <div class="card-body">
+                                      <p class="card-text text-muted small-xl">
+                                          <em class="ion-ios-calendar-outline"></em> 22h ago &nbsp;&nbsp;
+                                          <em class="ion-ios-person-outline"></em> Thao Nguyen &nbsp;&nbsp;
+                                          <em class="ion-ios-time-outline"></em> 5min read
+                                      </p>
+                                      <h5 class="card-title"><a href="#">What does it mean to be social?</a></h5>
+                                      <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Lorem ipsum dolor sit amet.</p>
+                                  </div>
+                                  <div class="card-body text-right">
+                                      <a href="#" class="card-link"><strong>Read more</strong></a>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-sm-4 mt-2 wow fadeIn">
+                              <div class="card">
+                                  <img class="card-img-top" src="https://media.istockphoto.com/id/517131390/photo/happy-young-friends-partying-with-drinks-and-confetti-in-nightclub.jpg?s=612x612&w=0&k=20&c=t0bhMdoWYz2XQiimDfVBJgiRvkqbwx-g8SH9oMFGpyk=" alt="Image">
+                                  <div class="card-body">
+                                      <p class="card-text text-muted small-xl">
+                                          <em class="ion-ios-calendar-outline"></em> 1week ago &nbsp;&nbsp;
+                                          <em class="ion-ios-person-outline"></em> Thao Nguyen &nbsp;&nbsp;
+                                          <em class="ion-ios-time-outline"></em> 2min read
+                                      </p>
+                                      <h5 class="card-title"><a href="#">Wild: An Unforgettable Launch Event</a></h5>
+                                      <p class="card-text">An editorial from Mob CEO on the success of the app's first event and launch party.</p>
+                                  </div>
+                                  <div class="card-body text-right">
+                                      <a href="#" class="card-link"><strong>Read more</strong></a>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-sm-4 mt-2 wow fadeIn">
+                              <div class="card">
+                                  <img class="card-img-top" src="https://media.istockphoto.com/id/517131390/photo/happy-young-friends-partying-with-drinks-and-confetti-in-nightclub.jpg?s=612x612&w=0&k=20&c=t0bhMdoWYz2XQiimDfVBJgiRvkqbwx-g8SH9oMFGpyk=" alt="Image">
+                                  <div class="card-body">
+                                      <p class="card-text text-muted small-xl">
+                                          <em class="ion-ios-calendar-outline"></em> 1mon ago &nbsp;&nbsp;
+                                          <em class="ion-ios-person-outline"></em> Thao Nguyen &nbsp;&nbsp;
+                                          <em class="ion-ios-time-outline"></em> 10min read
+                                      </p>
+                                      <h5 class="card-title"><a href="#">Mob Beta 1.0: What's New</a></h5>
+                                      <p class="card-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Lorem ipsum dolor sit amet.</p>
+                                  </div>
+                                  <div class="card-body text-right">
+                                      <a href="#" class="card-link"><strong>Read more</strong></a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
             </section>
       </div>
     );
