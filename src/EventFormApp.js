f79daa93dcd17a78da1b759b39b6b7b14572a83e
CoSphere24 <cosphere24@gmail.com>
2025-04-03 19:37:03 -0400

    Update EventFormApp.js

--- src/EventFormApp.js
+++ src/EventFormApp.js
@@ -163,12 +163,12 @@ function EventFormApp() {
                   type='checkbox'
                   label='This is a recurring event.'
                 />
-                <Form.Select aria-label="Default select example">
+                /*<Form.Select aria-label="Default select example">
                   <option>Open this select menu</option>
                   <option value="1">One</option>
                   <option value="2">Two</option>
                   <option value="3">Three</option>
-                </Form.Select>
+                </Form.Select>*/
               </Form.Group>
             </Col>
           </Row>
