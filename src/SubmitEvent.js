56f6888ed81a7eac5a86202bccab2e1e41434386
CoSphere24 <cosphere24@gmail.com>
2025-04-03 20:01:18 -0400

    Update SubmitEvent.js

--- src/SubmitEvent.js
+++ src/SubmitEvent.js
@@ -8,7 +8,7 @@ import './bootstrap.css';
 
 const SubmitEvent = () => (
       <div className='SubmitEvent'>
-            <section style={{background-color:orange}}></section>
+            <section style={{backgroundColor:#ff7842}}></section>
             <section class="bg-light">
                   <br/><br/>
                   <div class="container">
