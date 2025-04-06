4f57678c59de72e20e42b02b87d8c378afe1b307
CoSphere24 <cosphere24@gmail.com>
2025-04-06 00:34:35 -0400

    Update EventFormApp.js

--- src/EventFormApp.js
+++ src/EventFormApp.js
@@ -230,7 +230,7 @@ function EventFormApp() {
           <Row>
             <Col sm={6} style={{ marginBottom: '10px' }}>
               <Form.Group controlId="formFile" className="mb-3">
-                <FloatingLabel controlId='eventImageLabel' label='Event image <br> (must be horizontal 16:9 ratio, min 1600 x 900 px)'>
+                <FloatingLabel controlId='eventImageLabel' label='Event image <br></br> (must be horizontal 16:9 ratio, min 1600 x 900 px)'>
                   <Form.Control type="file" required />
                 </FloatingLabel>
               </Form.Group>
